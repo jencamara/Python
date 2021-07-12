dedf0fbeaa1a27949ae20dc40d6bc5254402bd6e
jencamara <jencamagong@gmail.com>
2021-07-11 17:39:02 -0700

    W1D5 Commit

--- flask/fundamentals/Playground/server.py
+++ flask/fundamentals/Playground/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def play():
     return render_template('index.html')
 
+@app.route('/play/<int:x>')
+def play(x):
+    return render_template('lvl_2_index.html', box = x)
+
 if __name__=="__main__":
     app.run(debug=True)
